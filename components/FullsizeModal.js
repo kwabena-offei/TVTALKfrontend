782084cb91a566827d8a15f191f5e993ef61bd27
Svitlana Sytnyk <svitlana@nxte.nl>
2022-11-21 21:19:03 +0200

    Minor style fix

--- components/FullsizeModal.js
+++ components/FullsizeModal.js
@@ -8,9 +8,6 @@ import BackButton from './BackButton';
 
 const style = {
   position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
   width: '100vw',
   height: '100vh',
   bgcolor: '#090F27',
