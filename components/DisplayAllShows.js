13765932ad0d24048b089d46a29ce30f7808515e
Dylan Montgomery <mail@citizensinspace.com>
2023-08-26 12:13:58 -0400

    Update homepage category title spacing

--- components/DisplayAllShows.js
+++ components/DisplayAllShows.js
@@ -11,38 +11,39 @@ import HeartButton from '../components/HeartButton';
 import BlueButton from '../components/BlueButton';
 
 const DisplayAllShows = ({ shows }) => {
-    const router = useRouter()
+  const categories = shows;
+  const router = useRouter()
 
-    const convertToSlug = (name) => {
-        let slug = ''
-        slug = name.replace('%20', '-').toLowerCase()
-        return slug
-    }
+  const convertToSlug = (name) => {
+    let slug = ''
+    slug = name.replace('%20', '-').toLowerCase()
+    return slug
+  }
 
-    // Pushes tmsID to the about page
-    const handleAbout = (tmsId, title) => {
+  // Pushes tmsID to the about page
+  const handleAbout = (tmsId, title) => {
 
-        router.push({ pathname: '/about', query: { tmsId: tmsId } })
-    }
-    const handleChat = (tmsId, title) => {
+    router.push({ pathname: '/about', query: { tmsId: tmsId } })
+  }
+  const handleChat = (tmsId, title) => {
 
-        router.push({ pathname: '/chat/[tmsId]', query: { tmsId: tmsId } })
-    }
+    router.push({ pathname: '/chat/[tmsId]', query: { tmsId: tmsId } })
+  }
 
-    return (
-        <>
-            <Box style={{ position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Box style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 4 }}>
-                    <Typography sx={{ color: 'white' }}>Let's start a community of TV fans </Typography>
-                    <Typography sx={{ color: 'white' }}>Press "Chat" and post a message!</Typography>
-                </Box>
-                <Box style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, background: 'rgba(9, 15, 39, 0.32)', }} />
-                <img src='/assets/header.jpg' width='100%' />
-            </Box>
-            <Box className='wrapper'>
+  return (
+    <>
+      <Box style={{ position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 4 }}>
+          <Typography sx={{ color: 'white' }}>Let's start a community of TV fans </Typography>
+          <Typography sx={{ color: 'white' }}>Press "Chat" and post a message!</Typography>
+        </Box>
+        <Box style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, background: 'rgba(9, 15, 39, 0.32)', }} />
+        <img src='/assets/header.jpg' width='100%' />
+      </Box>
+      <Box className='wrapper'>
 
 
-                {/* <Accordion sx={{
+        {/* <Accordion sx={{
                     boxShadow: 'none',
                     padding: 0,
                     '& .MuiCollapse-root': {
@@ -65,65 +66,66 @@ const DisplayAllShows = ({ shows }) => {
                         <Box sx={{ background: '' }}></Box>
                     </AccordionDetails>
                 </Accordion> */}
-                {shows.map((show, index) =>
-                    <div key={index} style={{ margin: '100px 0' }}>
+        {categories.map((category, index) =>
+          <div key={index} style={{ margin: '100px 0' }}>
+            <div style={{ marginLeft: 50, paddingLeft: 20, marginBottom: 20 }}>
+              <Typography style={{
+                color: '#EFF2FD',
+                fontFamily: 'Gilroy',
+                fontSize: 40,
+                fontWeight: 700,
+                lineHeight: '130%', /* 52px */
+                letterSpacing: 0.4,
+              }}>{category.title}</Typography></div>
+            <Carousel itemsToShow={4} itemsToScroll={4} pagination={false} itemPadding={[0, 10]}>
 
+              {category.shows.map((tvShow, ind) => {
+                return <Card key={ind} sx={{ background: 'transparent' }}>
+                  <CardMedia
+                    component="img"
+                    src={`https://${tvShow.preferred_image_uri}`}
 
+                  />
+                  <CardContent sx={{ background: '#131B3F' }}>
+                    <Typography sx={{ color: '#EFF2FD' }} gutterBottom variant="h5" component="div">
+                      {tvShow.title}
+                    </Typography>
+                    <Grid container spacing={1}>
+                      <Grid item>
+                        <BlueButton
+                          title='Chat'
+                          onClick={() => handleChat(tvShow.tmsId)}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button onClick={() => handleAbout(tvShow.tmsId)} style={{ background: '#090F27', borderRadius: '10000px', boxShadow: 'none' }} variant='contained'>
+                          <Typography sx={{ color: '#919CC0' }} variant='string'>About</Typography>
+                        </Button>
+                      </Grid>
+                      <Grid item>
+                        <HeartButton />
+                      </Grid>
+                    </Grid>
+                  </CardContent>
 
+                </Card>
+              }
 
+              )}
+            </Carousel>
 
-
-                        <Typography></Typography>
-                        <Carousel itemsToShow={4} itemsToScroll={4} pagination={false} itemPadding={[0, 10]}>
-
-                            {show.shows.map((tvShow, ind) => {
-                                return <Card key={ind} sx={{ background: 'transparent' }}>
-                                    <CardMedia
-                                        component="img"
-                                        // src="wewe.tmsimg.com/assets/p13909803_b_h9_am.jpg?w=720&h=540"
-                                        src={`https://${tvShow.preferred_image_uri}`}
-
-                                    />
-                                    <CardContent sx={{ background: '#131B3F' }}>
-                                        <Typography sx={{ color: '#EFF2FD' }} gutterBottom variant="h5" component="div">
-                                            {tvShow.title}
-                                        </Typography>
-                                        <Grid container spacing={1}>
-                                            <Grid item>
-                                                <BlueButton
-                                                    title='Chat'
-                                                    onClick={() => handleChat(tvShow.tmsId)}
-                                                />
-                                            </Grid>
-                                            <Grid item>
-                                                <Button onClick={() => handleAbout(tvShow.tmsId)} style={{ background: '#090F27', borderRadius: '10000px', boxShadow: 'none' }} variant='contained'>
-                                                    <Typography sx={{ color: '#919CC0' }} variant='string'>About</Typography>
-                                                </Button>
-                                            </Grid>
-                                            <Grid item>
-                                                <HeartButton />
-                                            </Grid>
-                                        </Grid>
-                                    </CardContent>
-
-                                </Card>
-                            }
-
-                            )}
-                        </Carousel>
-
-                        {/* </AccordionDetails>
+            {/* </AccordionDetails>
                     </Accordion> */}
 
 
 
-                    </div>
-                )}
-            </Box>
-        </>
+          </div>
+        )}
+      </Box >
+    </>
 
 
-    );
+  );
 };
 
 export default DisplayAllShows;
