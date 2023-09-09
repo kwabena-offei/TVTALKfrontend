00e7475a4a854004a0706c8ffc113abd8f3b9579
Dylan Montgomery <mail@citizensinspace.com>
2023-09-09 19:03:44 -0400

    Implement ratings on about page

--- components/RatingButtonsGroup.js
+++ components/RatingButtonsGroup.js
@@ -7,53 +7,67 @@ import RollingEyesSmile from './RollingEyesSmile';
 import { styled } from '@mui/system';
 
 const StyledTitle = styled(Typography, {})
-    ({
-        color: '#EFF2FD',
-        fontSize: '36px',
-        lineHeight: '43px',
-        marginRight: '30px', 
-        ['@media (max-width:780px)'] : {
-            fontSize: '20px',
-            lineHeight: '28px',
-          }
-    });
+  ({
+    color: '#EFF2FD',
+    fontSize: '36px',
+    marginRight: '30px',
+    ['@media (max-width:780px)']: {
+      fontSize: '20px',
+      lineHeight: '28px',
+    }
+  });
 
-    const StyledButtonsWrapper = styled(Box, {})
-    ({
-        ['@media (max-width:780px)'] : {
-            display: 'flex',
-        }
-    });
+const StyledButtonsWrapper = styled(Box, {})
+  ({
+    ['@media (max-width:780px)']: {
+      display: 'flex',
+    }
+  });
 
-const RatingButtonsGroup = ({ love, like, dislike }) => {
-    return (
-        <Box
-            sx={{ marginTop: '60px' }}
-        >
-            <StyledTitle 
-                variant='string'>
-                    {'Ratings'}
-            </StyledTitle>
-            <StyledButtonsWrapper>
-                <RatingButton
-                    icon={<HeartsSmile />}
-                    title='Love it'
-                    rating={love ? `${love}%` : ''}
-                    checked={true}
-                />
-                <RatingButton
-                    icon={<StarSmile />}
-                    title='Like it'
-                    rating={like ? `${like}%` : ''}
-                />
-                <RatingButton
-                    icon={<RollingEyesSmile />}
-                    title='Leave it'
-                    rating={dislike ? `${dislike}%` : ''}
-                />
-            </StyledButtonsWrapper>
-        </Box>
-    );
+const RatingButtonsGroup = ({ love, like, dislike, tmsId, onRate, userRating }) => {
+
+  return (
+    <Box
+      sx={{ marginTop: '60px', display: 'flex', alignItems: 'flex-end' }}
+    >
+      <StyledTitle
+        variant='string'>
+        <Typography sx={{
+          fontSize: '36px',
+          lineHeight: '47px',
+          color: '#EFF2FD',
+          fontWeight: '600',
+          marginBottom: '16px'
+        }}>
+          {'Ratings'}
+        </Typography>
+      </StyledTitle>
+
+      <StyledButtonsWrapper>
+        <RatingButton
+          icon={<HeartsSmile />}
+          title='Love it'
+          rating={love ? `${love}%` : ''}
+          checked={userRating === 'love'}
+          onClick={onRate.bind(this, 'love')}
+        />
+        <RatingButton
+          icon={<StarSmile />}
+          title='Like it'
+          rating={like ? `${like}%` : ''}
+          checked={userRating === 'like'}
+          onClick={onRate.bind(this, 'like')}
+        />
+        <RatingButton
+          icon={<RollingEyesSmile />}
+          title='Leave it'
+          rating={dislike ? `${dislike}%` : ''}
+          checked={userRating === 'dislike'}
+          onClick={onRate.bind(this, 'dislike')}
+        />
+      </StyledButtonsWrapper>
+    </Box>
+  );
 };
 
 export default RatingButtonsGroup;
