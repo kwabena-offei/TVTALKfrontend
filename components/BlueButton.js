fa03e7df16dd4dd532fd0473d88838d7e063f1e1
Dylan Montgomery <mail@citizensinspace.com>
2023-09-09 10:40:28 -0400

    Styled about page hero image

--- components/BlueButton.js
+++ components/BlueButton.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Button, Typography } from '@mui/material';
 
 const BlueButton = ({ title, ...props }) => {
-    return (
-        <Button style={{ background: '#3361FF', borderRadius: '10000px' }} variant='contained' {...props}>
-            <Typography sx={{ color: '#EFF2FD' }} variant='string'>{title}</Typography>
-        </Button>
-    );
+  return (
+    <Button style={{ background: '#3361FF', borderRadius: '10000px' }} variant='contained' {...props}>
+      <Typography sx={{ color: '#EFF2FD', fontSize: 16, fontWeight: 600, padding: '0px 8px' }} variant='string'>{title}</Typography>
+    </Button >
+  );
 };
 
 export default BlueButton;
--- components/CustomSelect.js
+++ components/CustomSelect.js
@@ -4,67 +4,67 @@ import { styled } from '@mui/system';
 import styles from './CustomSelect.module.scss';
 
 const StyledSelect = styled(Select, {})({
-    backgroundColor: '#131B3F',
-    height: '50px',
-    borderRadius: '38px',
-    borderWidth: '1.5px',
-    borderColor: '#131B3F',
-    padding: '12px 30px',
+  backgroundColor: '#131B3F',
+  height: '50px',
+  borderRadius: '38px',
+  borderWidth: '0',
+  borderColor: '#131B3F',
+  padding: '12px 30px',
 })
 
 const StyledInputLabel = styled(InputLabel, {})({
-    fontWeight: '400',
-    fontSize: '16px',
-    lineHeight: '26px',
-    color: '#EFF2FD',
-    left: '15px',
-    top: '-5px',
+  fontWeight: '400',
+  fontSize: '16px',
+  lineHeight: '26px',
+  color: '#EFF2FD',
+  left: '15px',
+  top: '-5px',
 })
 
 const CustomSelect = props => {
 
-    const { selectList, label, labelId, selectId, handleChange } = props;
+  const { selectList, label, labelId, selectId, handleChange } = props;
 
-    return (
-        <FormControl
-            fullWidth
-         >
-            <StyledInputLabel 
-                id={labelId}
-                sx={{
-                    '&.Mui-focused': {
-                        left: '-5px',
-                    }
-                  }}
-            >
-                {label}
-            </StyledInputLabel>
-            <StyledSelect
-                labelId={labelId}
-                id={selectId}
-                label={label}
-                onChange={handleChange}
-                sx={{
-                    '& .MuiSelect-icon': {
-                      color: '#3361FF',
-                      width: '22px',
-                      height: '22px',
-                      top: 'calc(50% - 11px)',
-                    },
-                  }}
-                MenuProps={{ classes: { paper: styles.paper} }}
-            >
-                {selectList.map((item, index) => (
-                    <MenuItem
-                        key={index}
-                        sx={{color: '#EFF2FD', fontSize: '16px'}}
-                    >
-                        {item}
-                    </MenuItem>
-                ) )}
-            </StyledSelect>
-        </FormControl>  
-    );
+  return (
+    <FormControl
+      fullWidth
+    >
+      <StyledInputLabel
+        id={labelId}
+        sx={{
+          '&.Mui-focused': {
+            left: '-5px',
+          }
+        }}
+      >
+        {label}
+      </StyledInputLabel>
+      <StyledSelect
+        labelId={labelId}
+        id={selectId}
+        label={label}
+        onChange={handleChange}
+        sx={{
+          '& .MuiSelect-icon': {
+            color: '#3361FF',
+            width: '22px',
+            height: '22px',
+            top: 'calc(50% - 11px)',
+          },
+        }}
+        MenuProps={{ classes: { paper: styles.paper } }}
+      >
+        {selectList.map((item, index) => (
+          <MenuItem
+            key={index}
+            sx={{ color: '#EFF2FD', fontSize: '16px' }}
+          >
+            {item}
+          </MenuItem>
+        ))}
+      </StyledSelect>
+    </FormControl>
+  );
 };
 
 
