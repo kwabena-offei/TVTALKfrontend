370dbbbf8edff448d6a4c1ba31c41c3cd9040466
Dylan Montgomery <mail@citizensinspace.com>
2023-09-09 10:40:51 -0400

    Fixed about page show photos

--- components/CastSlider.js
+++ components/CastSlider.js
@@ -6,38 +6,38 @@ import { useWindowDimensions } from '../util/useWindowDimensions.js';
 
 const CastSlider = ({ cast }) => {
 
-    const { isMobile } = useWindowDimensions();
+  const { isMobile } = useWindowDimensions();
 
-    return (
-        <Box
-            sx={{ marginTop: '60px' }}
-        >
-            <Typography sx={{ 
-                    fontSize: '36px',
-                    lineHeight: '47px',
-                    color: '#EFF2FD',
-                    fontWeight: '600',
-                    marginBottom: '16px'
-                }}>
-                    {'Cast'}
-                </Typography>
-            <Carousel 
-                itemsToShow={isMobile ? 2 : 5}
-                itemsToScroll={2}
-                pagination={false}
-                itemPadding={[0, 10]}
-                showArrows={!isMobile}
-            >
-                {cast.map((actor, index) => (
-                    <ActorCard
-                        key={index}
-                        name={actor.name}
-                        characterName={actor.characterName}
-                    />
-                ))}
-            </Carousel>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{ marginTop: '60px' }}
+    >
+      <Typography sx={{
+        fontSize: '36px',
+        lineHeight: '47px',
+        color: '#EFF2FD',
+        fontWeight: '600',
+        marginBottom: '16px'
+      }}>
+        {'Cast'}
+      </Typography>
+      <Carousel
+        itemsToShow={isMobile ? 2 : 5}
+        itemsToScroll={2}
+        pagination={false}
+        itemPadding={[0, 10]}
+        showArrows={!isMobile}
+      >
+        {cast.map((actor, index) => (
+          <ActorCard
+            key={index}
+            name={actor.name}
+            characterName={actor.characterName}
+          />
+        ))}
+      </Carousel>
+    </Box>
+  );
 };
 
 export default CastSlider;
