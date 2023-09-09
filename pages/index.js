3a0a139da61ff692fa86d4d89b72747114ebfd6b
Dylan Montgomery <mail@citizensinspace.com>
2023-09-09 16:50:00 -0400

    cache homepage

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,11 @@ export default function Home({ categories }) {
   )
 }
 
-Home.getInitialProps = async (ctx) => {
-  const res = await fetch('https://api.tvtalk.app/categories')
+Home.getInitialProps = async ({ res }) => {
+  // Set cache for 12 hours
+  res.setHeader('Cache-Control', 'public, s-maxage=43200, stale-while-revalidate=86400');
+  const categoryResponse = await fetch('https://api.tvtalk.app/categories')
+  const json = await categoryResponse.json()
 
-  const json = await res.json()
   return { categories: json }
 }
