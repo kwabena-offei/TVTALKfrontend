af30a9768fde4d7a1f67f9b214875f20403cd56f
kenzcode <kenleecoder@gmail.com>
2024-02-08 18:55:40 -0500

    fixed the season/episode selector issue

--- components/SeasonEpisodeSelector.js
+++ components/SeasonEpisodeSelector.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import CustomSelect from './CustomSelect';
+import React, { useState } from "react";
+import CustomSelect from "./CustomSelect";
 import useAxios from "../services/api";
 
 const SeasonEpisodeSelector = ({ tmsId, totalSeasons }) => {
@@ -7,8 +7,8 @@ const SeasonEpisodeSelector = ({ tmsId, totalSeasons }) => {
   const seasonList = Array.from({ length: parseInt(totalSeasons) }, (_, i) => {
     return {
       value: i + 1,
-      label: `Season ${i + 1}`
-    }
+      label: `Season ${i + 1}`,
+    };
   });
 
   const [season, setSeason] = useState(null);
@@ -18,42 +18,52 @@ const SeasonEpisodeSelector = ({ tmsId, totalSeasons }) => {
 
   const handleSeasonChange = async (event) => {
     setSeason(event.target.value);
-    const { data: episodes } = await axios.get(`data/v1.1/series/${tmsId}/episodes?tms_id=${tmsId}&season=${season}&titleLang=en&descriptionLang=en`);
 
-
-    const episodeList = episodes.map(({ tmsId, episodeNum, episodeTitle }) => {
-      return {
-        value: tmsId,
-        label: `${episodeNum} - ${episodeTitle}`
-      }
-    });
-    setEpisodes(episodeList);
-    setEpisode(episodeList[0]);
-    setNoSelectedSeason(true);
-  }
+    try {
+      const { data: episodes } = await axios.get(
+        `data/v1.1/series/${tmsId}/episodes?tms_id=${tmsId}&season=${event.target.value}&titleLang=en&descriptionLang=en`
+      );
+      const episodeList =
+        episodes?.map(({ tmsId, episodeNum, episodeTitle }) => {
+          return {
+            value: tmsId,
+            label: `${episodeNum} - ${episodeTitle}`,
+          };
+        }) || [];
+      setEpisodes(episodeList);
+      setEpisode(episodeList[0]);
+      setNoSelectedSeason(true);
+    } catch (error) {}
+  };
 
   const handleEpisodeChange = (event) => {
     setEpisode(event.target.value);
-  }
+  };
 
   return (
     <>
       <CustomSelect
         selectList={seasonList}
-        label='Season Selector'
-        labelId='selectSeason'
-        selectId='selectSeason'
+        label="Season Selector"
+        labelId="selectSeason"
+        selectId="selectSeason"
         handleChange={handleSeasonChange}
       />
-      {noSelectedSeason? <><CustomSelect
-        selectList={episodes}
-        label='Select Episode'
-        labelId='selectEpisode'
-        selectId='selectEpisode'
-        handleChange={handleEpisodeChange}
-      /></>: <div style={{width: '100%'}}></div>}
+      {noSelectedSeason ? (
+        <>
+          <CustomSelect
+            selectList={episodes}
+            label="Select Episode"
+            labelId="selectEpisode"
+            selectId="selectEpisode"
+            handleChange={handleEpisodeChange}
+          />
+        </>
+      ) : (
+        <div style={{ width: "100%" }}></div>
+      )}
     </>
   );
-}
+};
 
 export default SeasonEpisodeSelector;
