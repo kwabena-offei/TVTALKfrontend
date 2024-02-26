b88783bf163c07a0eb0a25778219b451cee4c5a9
hugoth <htessierhuort@gmail.com>
2024-02-26 15:56:30 +0100

    reformat code

--- components/ReactionCard/PopupActions/ListActions.js
+++ components/ReactionCard/PopupActions/ListActions.js
@@ -7,36 +7,47 @@ import UnfollowIcon from "../../Icons/UnfollowIcon";
 import Report from "../Report";
 import { useRouter } from "next/router";
 import { TV_TALK_HOST, TV_TALK_HOST_LOCAL } from "../../../util/constants";
-import { copyToClipboard } from '../../../util/helpers';
-import getConfig from 'next/config';
+import { copyToClipboard } from "../../../util/helpers";
+import getConfig from "next/config";
 import { AuthContext } from "../../../util/AuthContext";
 import { ShareContext } from "../../../util/ShareContext";
 
-export const ListActions = ({ handleClose, commentType, id, tmsId, header }) => {
-  const router = useRouter()
+export const ListActions = ({
+  handleClose,
+  commentType,
+  id,
+  tmsId,
+  header,
+}) => {
+  const router = useRouter();
   const { publicRuntimeConfig } = getConfig();
   const isAuth = useContext(AuthContext);
   const { openShare, setOpenShare } = useContext(ShareContext);
 
-  const baseUrl = publicRuntimeConfig.API_ENV === 'development' ? TV_TALK_HOST_LOCAL : TV_TALK_HOST;
-  const copyLink = header ? `${baseUrl}${router.asPath}` : `${baseUrl}${router.asPath}#${id}`
-  
-  const toggleShare = () => setOpenShare(!openShare)
+  const baseUrl =
+    publicRuntimeConfig.API_ENV === "development"
+      ? TV_TALK_HOST_LOCAL
+      : TV_TALK_HOST;
+  const copyLink = header
+    ? `${baseUrl}${router.asPath}`
+    : `${baseUrl}${router.asPath}#${id}`;
+
+  const toggleShare = () => setOpenShare(!openShare);
   const handleShareClose = (event) => {
-    handleClose(event)
-    toggleShare()
-  }
+    handleClose(event);
+    toggleShare();
+  };
   const handleCopyLink = async (event) => {
     await copyToClipboard(copyLink);
-    handleClose(event)
-  }
+    handleClose(event);
+  };
   const handleUnfollow = async (event) => {
     // Todo: find api endpoint to change the status of following
     if (isAuth) {
-      console.log('you can unfollow', id)
+      console.log("you can unfollow", id);
     }
-    handleClose(event)
-  }
+    handleClose(event);
+  };
 
   return (
     <>
